6dda7c872ce4f3232ee3b50cf25970cbc1c4c7be
nam0mi <97060752+nam0mi@users.noreply.github.com>
2023-09-30 19:38:33 -0700

    adding a form and deleting preloaded data

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,6 +1,7 @@
 // src/MyApp.js
 import React, {useState} from "react";
 import Table from "./Table";
+import Form from './Form';
 
 // const characters = [
 //   {
@@ -22,24 +23,7 @@ import Table from "./Table";
 // ];
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-      {
-        name: 'Charlie',
-        job: 'Janitor', 
-      },
-      {
-        name: "Mac",
-        job: "Bouncer",
-      },
-      {
-        name: "Dee",
-        job: "Aspring actress",
-      },
-      {
-        name: "Dennis",
-        job: "Bartender",
-      },
-    ]); 
+  const [characters, setCharacters] = useState([]); 
 
   // function removeCharacter(index) {
   //   const updatedCharacters = characters.filter((character, i) => i !== index);
@@ -57,6 +41,7 @@ return (
   <div className="container">
       <Table characterData={characters} 
         removeCharacter={removeOneCharacter} />
+      <Form />
   </div>  
 ) 
 }
