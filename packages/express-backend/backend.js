94c1550dd1e460922a4dd0237dfc1c2da9763bd0
nam0mi <97060752+nam0mi@users.noreply.github.com>
2023-10-05 14:16:03 -0700

    added find user by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -38,6 +38,10 @@ const findUserByName = (name) => {
         .filter( (user) => user['name'] === name); 
 }
 
+const findUserById = (id) =>
+    users['users_list']
+        .find( (user) => user['id'] === id);
+
 app.use(express.json());
 
 app.get('/', (req, res) => { //setting up first API endpoint
@@ -53,6 +57,16 @@ app.get('/users', (req, res) => {
     }
     else{
         res.send(users);
+    }
+});
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send('Resource not found.');
+    } else {
+        res.send(result);
     }
 });
 
