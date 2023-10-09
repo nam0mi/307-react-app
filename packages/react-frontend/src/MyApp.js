88089eba8898bf27a5c42f5e28e384d557f091dc
nam0mi <97060752+nam0mi@users.noreply.github.com>
2023-10-08 18:46:24 -0700

    created postUser function

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -25,8 +25,12 @@ import Form from './Form';
 function MyApp() {
   const [characters, setCharacters] = useState([]); 
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
+  function updateList(person) { 
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      })
   }
   
   function removeOneCharacter (index) {
@@ -38,6 +42,18 @@ function MyApp() {
 
   function fetchUsers() {
     const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
     return promise;
   }
 
